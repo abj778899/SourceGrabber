941dce056e18abdcf9704b50d3d8d89b7fd02207
abj778899 <122936326@qq.com>
2026-09-01 02:08:50 +0800

    Update SourceGrabber/SourceGrabberApp.swift

--- SourceGrabber/SourceGrabberApp.swift
+++ SourceGrabber/SourceGrabberApp.swift
@@ -2,12 +2,9 @@ import SwiftUI
 
 @main
 struct SourceGrabberApp: App {
-    @StateObject private var historyStore = HistoryStore()
-    
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(historyStore)
         }
     }
 }
